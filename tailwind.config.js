98bc685e8036822bb2963de9f58be186f5debc28
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-01-31 09:09:26 -0500

    Avance de maquetación mundoweb

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+
+      colors: {
+        primary: '#3490dc', // Puedes cambiar el código de color según tus preferencias
+        fondo: '#1e2f33',
+        fondoboton: '#FFFFFF66',
+      },
+
+    },
+  },
+  plugins: [],
+}
+
